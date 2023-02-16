fd75d5fd7801199b501f08502f40ee708a8b6a30
miladsamy <miladsamy04@gmail.com>
2023-02-16 15:38:48 -0800

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 /*
- * main - pirntf the size of various typesa based on 
- *the computer it is compiled and run on..
+ * main - main function 
+ *
  * Return: 0
  */
+
 int main(void)
 {
 printf("Size of a char: %zu byte(s)\n", sizeof(char));
 printf("Size of an int: %zu byte(s)\n", sizeof(int));
 printf("Size of a long int: %zu byte(s)\n", sizeof(long int));
 printf("Size of a long long int: %zu byte(s)", sizeof(long long int));
-printf("Size of a float: %zubyte(s)\n", sizeof(float));
+printf("Size of a float: %zu byte(s)\n", sizeof(float));
 return (0);
 }
