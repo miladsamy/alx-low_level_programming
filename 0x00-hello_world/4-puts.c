682ccbf4530efd6898fa570f052a34068f395f5e
miladsamy <miladsamy04@gmail.com>
2023-02-16 11:57:58 -0800

    task4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,16 +1,13 @@
 #include <stdio.h>
-<<<<<<< HEAD
+
 /**
  * *main - this is the main function
  *
  * Return: always 0
  */
-int main(void){
-	puts("\"Programming is like building a multilingual puzzle");
-	return(0);
-=======
+
 int mani(void){
 	puts("\"Programming is like building a multilingual puzzle");
 	return(0);	
->>>>>>> f7d456dac9c6baea00ff3b654340e7766c35ccf0
+
 }
