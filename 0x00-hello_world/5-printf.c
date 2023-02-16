676434eb427ef5ebadd63e66a7c1dac72ea2190b
miladsamy <miladsamy04@gmail.com>
2023-02-16 13:45:34 -0800

    task5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,11 +3,12 @@
 /**
  * main - entry point
  *
- * Return: 0 when successfull
+ * Return: 0
  */
 
 int main(void)
 {
-        printf("with proper grammar, but the outcome is a piece of art,\n");
+        puts("with proper grammar, but the outcome is a piece of art,\n");
         return (0);
 }
+
