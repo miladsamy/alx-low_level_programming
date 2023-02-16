d439283ff98b6139c90d1b50c00661e3d395d8e0
miladsamy <miladsamy04@gmail.com>
2023-02-16 13:39:24 -0800

    task5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,4 +1,4 @@
-#include <stdio.h>		
+#include <stdio.h>
 
 /**
  * main - entry point
@@ -8,6 +8,6 @@
 
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+        printf("with proper grammar, but the outcome is a piece of art,\n");
+        return (0);
 }
